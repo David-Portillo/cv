3893b4f4496d5d0c40ecb2d4c2e4ed8bdb795350
HammerHand <david.portillo92@gmail.com>
2019-07-22 21:07:58 -0400

    added muiTheme for navigation

--- react-cv/src/components/Navigation.js
+++ react-cv/src/components/Navigation.js
@@ -2,22 +2,25 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { navigationStyle } from '../styles/navigation-style';
+import { navigationStyle, theme } from '../styles/navigation-style';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
 const Navigation = (props) => {
 	const { classes } = props;
 	return (
-		<div className={classes.root}>
-			<AppBar position="static" color="default">
-				<Toolbar>
-					<Typography variant="h6" color="inherit">
-						My Bio Application
-					</Typography>
-				</Toolbar>
-			</AppBar>
-		</div>
+		<MuiThemeProvider theme={theme}>
+			<div className={classes.root}>
+				<AppBar position="static" color="secondary">
+					<Toolbar>
+						<Typography variant="h6" color="inherit">
+							My Bio Application
+						</Typography>
+					</Toolbar>
+				</AppBar>
+			</div>
+		</MuiThemeProvider>
 	);
 };
 
--- react-cv/src/styles/navigation-style.js
+++ react-cv/src/styles/navigation-style.js
@@ -1,5 +1,15 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+
 export const navigationStyle = (theme) => ({
 	navRoot: {
 		flexGrow: 1
+	}
+});
+
+export const theme = createMuiTheme({
+	palette: {
+		primary: { main: purple[500] }, // Purple and green play nicely together.
+		secondary: { main: '#11cb5f' } // This is just green.A700 as hex.
 	}
 });
