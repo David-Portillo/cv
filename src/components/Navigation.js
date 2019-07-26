c588c96df416da4c2f666034dc3bb880a1113dea
David Portillo <davidportillo@pop-os.localdomain>
2019-07-26 10:25:25 -0400

    removed & added material ui and references semantic-ui-react package

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,31 +1,13 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import { navigationStyle, theme } from '../styles/navigation-style';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { MuiThemeProvider } from '@material-ui/core/styles';
 
 const Navigation = (props) => {
-	const { classes } = props;
 	return (
-		<MuiThemeProvider theme={theme}>
-			<div className={classes.root}>
-				<AppBar position="static" color="primary">
-					<Toolbar>
-						<Typography variant="h6" color="inherit">
-							My Bio Application
-						</Typography>
-					</Toolbar>
-				</AppBar>
+			<div>
+			Header
 			</div>
-		</MuiThemeProvider>
+
 	);
 };
 
-Navigation.propTypes = {
-	classes : PropTypes.object.isRequired
-};
 
-export default withStyles(navigationStyle)(Navigation);
+export default Navigation;
