4fa66d02fd8808aac06ec0bdf4a0a6dcc3073509
HammerHand <david.portillo92@gmail.com>
2019-07-26 23:40:47 -0400

    change format using prettier now

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react';
 
 const Navigation = (props) => {
 	return (
-	<Menu>
-		<Menu.Item header>Bio</Menu.Item>
-	</Menu>
-
+		<Menu>
+			<Menu.Item header>Bio</Menu.Item>
+		</Menu>
 	);
 };
-
 
 export default Navigation;
