9b3e7e7d33efba78be837f61aba47a798b1e7616
HammerHand <david.portillo92@gmail.com>
2019-07-27 01:06:14 -0400

    added a card placeholder

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,34 +1,60 @@
 import React, { Component } from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Card, Icon, Image, Container, Header, Grid } from 'semantic-ui-react';
 
 class Summary extends Component {
 	render() {
 		return (
-			<Container text>
-				<Header>Summary</Header>
-				<p>
-					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
-					massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-					Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis
-					enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-					imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer
-					tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo
-					ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra
-					quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
-					imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-				</p>
-				<p>
-					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
-					massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-					Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis
-					enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-					imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer
-					tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo
-					ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra
-					quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
-					imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-				</p>
-			</Container>
+			<div>
+				<Grid centered>
+					<Grid.Row>
+						<Card>
+							<Image src="https://via.placeholder.com/250" />
+							<Card.Content>
+								<Card.Header>David Portillo</Card.Header>
+								<Card.Meta>
+									<span className="date">my little subtitle</span>
+								</Card.Meta>
+								<Card.Description>David is a software developer.</Card.Description>
+							</Card.Content>
+							<Card.Content extra>
+								<Icon name="user" />
+							</Card.Content>
+						</Card>
+					</Grid.Row>
+
+					<Grid.Row>
+						<Container text textAlign="left">
+							<Header>Summary</Header>
+							<p>
+								Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
+								dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient
+								montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
+								pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+								aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis
+								vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
+								dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo
+								ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus
+								in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.
+								Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+								ultricies nisi.
+							</p>
+							<p>
+								Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
+								dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient
+								montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
+								pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+								aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis
+								vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
+								dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo
+								ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus
+								in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.
+								Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+								ultricies nisi.
+							</p>
+						</Container>
+					</Grid.Row>
+				</Grid>
+			</div>
 		);
 	}
 }
