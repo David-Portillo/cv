8d4feeac813185edeb1d6e28b57d7558ebddfa38
David Portillo <david.portillo92@gmail.com>
2022-11-05 22:25:55 -0400

    testing

--- src/js/main.js
+++ src/js/main.js
@@ -3,28 +3,41 @@
 const smallScreenAndDown = window.matchMedia("(max-width: 450px)");
 const mediumScreenAndUp = window.matchMedia("(min-width: 450px)");
 
+const branding = document.getElementById("branding");
+
 function updateBrandingStyle(screen) {
   console.log("in change");
   console.log(screen);
   const branding = document.getElementById("branding");
 
-  if (screen.matches) {
-    branding.style.opacity = "0";
-    branding.innerText = "DP.";
-    branding.style.opacity = "1";
-  } else {
-    branding.style.opacity = "0";
-    branding.innerText = "David Portillo.";
-    branding.style.opacity = "1";
-  }
+  // if (screen.matches) {
+  //   branding.style.opacity = "0";
+  //   branding.innerText = "DP.";
+  //   // branding.style.opacity = "1";
+  // } else {
+  //   // branding.style.opacity = "0";
+  //   branding.innerText = "David Portillo.";
+  //   // branding.style.opacity = "1";
+  // }
 }
 
 // Event Listeners
 
-smallScreenAndDown.addEventListener("change", (e) => {
-  updateBrandingStyle(e);
-});
+// smallScreenAndDown.addEventListener("change", (e) => {
+//   updateBrandingStyle(e);
+// });
 
 window.addEventListener("load", (e) => {
-  updateBrandingStyle(e);
+  // updateBrandingStyle(e);
+});
+
+window.addEventListener("resize", () => {
+  console.log(window.screen.availWidth);
+
+  if (window.innerWidth <= 550) {
+    // branding.style.opacity = "0";
+    branding.innerText = "DP.";
+  } else {
+    branding.innerText = "David Portillo.";
+  }
 });
