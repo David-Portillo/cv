2d83fa03bd41e805182d01f71aa176a48328b32f
David Portillo <david.portillo92@gmail.com>
2022-11-06 22:40:34 -0500

    cleanup code

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,28 @@
 const fullname = "David Portillo.";
 const abbreviatedName = "DP.";
 const mobileScreenWidth = 550;
+
 const brandingEl = document.getElementById("branding");
 
 function changeBrandingName() {
-  if (window.innerWidth <= mobileScreenWidth) {
-    return (brandingEl.innerText = abbreviatedName);
-  }
-  return (brandingEl.innerText = fullname);
+  brandingEl.style.transform = "translate(-500px)";
+
+  const resetBrandingPosition = () =>
+    (brandingEl.style.transform = "translate(0px)");
+
+  const interval = setInterval(() => {
+    if (window.innerWidth <= mobileScreenWidth) {
+      brandingEl.innerText = abbreviatedName;
+      resetBrandingPosition();
+    } else {
+      brandingEl.innerText = fullname;
+      resetBrandingPosition();
+    }
+    clearInterval(interval);
+  }, 500);
 }
 
-// Event Listeners
+// event Listeners
 
 window.addEventListener("load", () => {
   changeBrandingName();
