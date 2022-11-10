aaa25c2f3e709f20dd05d3b8c7951e82ea4fd788
David Portillo <david.portillo92@gmail.com>
2022-11-09 19:33:54 -0500

    added img radius, removed transitions

--- src/js/main.js
+++ src/js/main.js
@@ -5,20 +5,11 @@ function changeBrandingName() {
   const abbreviatedName = "DP.";
   const mobileScreenWidth = 550;
 
-  brandingEl.style.transform = "translate(-500px)";
-  const resetBrandingPosition = () =>
-    (brandingEl.style.transform = "translate(0px)");
-
-  const interval = setInterval(() => {
-    if (window.innerWidth <= mobileScreenWidth) {
-      brandingEl.innerText = abbreviatedName;
-      resetBrandingPosition();
-    } else {
-      brandingEl.innerText = fullname;
-      resetBrandingPosition();
-    }
-    clearInterval(interval);
-  }, 300);
+  if (window.innerWidth <= mobileScreenWidth) {
+    brandingEl.innerText = abbreviatedName;
+  } else {
+    brandingEl.innerText = fullname;
+  }
 }
 
 // event Listeners
