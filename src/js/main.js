cbca09642cf9c86bcc13e2fc7c8b28df69633fbd
David Portillo <david.portillo92@gmail.com>
2022-11-18 23:46:40 -0500

    cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,16 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
     const skillType = entry.target.getAttribute("data-skill");
-    console.log(skillType);
     const skill = entry.target.querySelector(`.${skillType}`);
-    // const skillJS = entry.target.querySelector(".html");
-    // console.log(skillJS);
-
-    // console.log(entry);
 
     if (entry.isIntersecting) {
-      skill.classList.add(`skill-${skillType}-animation`);
-      // skillJS.classList.add("skill-javascript-animation");
+      skill.classList.add(`${skillType}-animation`);
       return;
     }
-
-    // skillHTML.classList.remove("skill-html-animation");
-    // skillJS.classList.remove("skill-javascript-animation");
-
-    skill.classList.remove(`skill-${skillType}-animation`);
+    skill.classList.remove(`${skillType}-animation`);
   });
 });
 
-// observer.observe(document.querySelector(".html-skill-wrapper"));
-// observer.observe(document.querySelector(".js-skill-wrapper"));
-
-document.querySelectorAll(".skill-wrapper").forEach((element) => {
+document.querySelectorAll(".skill-container").forEach((element) => {
   observer.observe(element);
 });
