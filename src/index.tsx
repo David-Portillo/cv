2cd7ba59c29e072d88fadee64e4a5b3405423faa
David Portillo <david.portillo92@gmail.com>
2023-01-11 21:38:58 -0500

    config changes
    A - added new style
    M - moved code to new location

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./components/App";
+import App from "./components/launch/App";
 
-// import "./styles/scss/index.scss";
+import "./styles/index.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
