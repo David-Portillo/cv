9decadbeae0696123c07c4a38c1f8f1df290978f
David Portillo <david.portillo92@gmail.com>
2023-01-13 14:25:58 -0500

    added layout components

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = (): JSX.Element => {
+  return (
+    <footer>
+      <div className='social-media'></div>
+      <div className='contact'></div>
+      <div className='copyright'></div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = (): JSX.Element => {
+  return <header className='header'></header>;
+};
+
+export default Header;
