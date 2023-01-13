131f94f0d6cc614558a240a22321a386b8b504da
David Portillo <david.portillo92@gmail.com>
2023-01-13 14:26:24 -0500

    added new content section

---
+++ src/components/sections/Introduction.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Introduction = (): JSX.Element => {
+  return (
+    <div className='container'>
+      <h2 className='text'>Hi There!!!</h2>
+      <p className='text'>This is my introduction</p>
+    </div>
+  );
+};
+
+export default Introduction;
