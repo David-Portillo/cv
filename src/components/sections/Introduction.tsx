02c1203caac7affeac875bcada8c8d48aa8b30fe
David Portillo <david.portillo92@gmail.com>
2023-01-13 15:38:52 -0500

    updated intro section styles

--- src/components/sections/Introduction.tsx
+++ src/components/sections/Introduction.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Introduction = (): JSX.Element => {
   return (
-    <div className='container'>
-      <h2 className='text'>Hi There!!!</h2>
-      <p className='text'>This is my introduction</p>
+    <div className='section-container'>
+      <h2 className='faded-text'>Hi There!!!</h2>
+      <p className='faded-text'>This is my introduction</p>
     </div>
   );
 };
