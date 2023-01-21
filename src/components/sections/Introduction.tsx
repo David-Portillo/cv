9a89a3931979c619cf4f457ae5abca253463bf23
David Portillo <david.portillo92@gmail.com>
2023-01-20 23:18:31 -0500

    added an introduction

--- src/components/sections/Introduction.tsx
+++ src/components/sections/Introduction.tsx
@@ -4,7 +4,15 @@ const Introduction = (): JSX.Element => {
   return (
     <div className='section-container'>
       <h2 className='faded-text'>Hi There!!!</h2>
-      <p className='faded-text'>This is my introduction</p>
+      <p className='faded-text'>
+        Welcome to my software development portfolio! I am a highly motivated
+        and experienced developer with a passion for creating innovative and
+        efficient solutions. I have a strong background in various programming
+        languages and technologies, and have worked on a wide range of projects,
+        from web development to backend and more. I am excited to share my
+        skills and experience with you and look forward to the opportunity to
+        work on exciting new projects. Thank you for visiting my portfolio!
+      </p>
     </div>
   );
 };
