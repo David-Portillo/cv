48ee8dfb50cebfde1593732200e6a0ecdee48548
David Portillo <david.portillo92@gmail.com>
2023-03-13 20:32:57 -0400

    FEAT: updated introduction

--- src/components/sections/Introduction.tsx
+++ src/components/sections/Introduction.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
+import profile from "../../assets/images/photo-alt.svg";
+
 const Introduction = (): JSX.Element => {
   return (
     <React.Fragment>
       <div className='photo-grid-area'>
-        <div className='photo'>IMAGE</div>
+        <div className='photo'>
+          <img src={profile} alt='profile image' />
+        </div>
         <div className='assortment'>
           <div>ICON</div>
           <div>ICON</div>
@@ -12,16 +16,19 @@ const Introduction = (): JSX.Element => {
         </div>
       </div>
       <div className='intro-grid-area'>
-        <h2 className='paragraph-title'>Hi There!!!</h2>
+        <h2 className='paragraph-title'>Hi There!</h2>
         <p className='faded-paragraph'>
-          Welcome to my software development portfolio! I am a highly motivated
-          and experienced developer with a passion for creating innovative and
-          efficient solutions. I have a strong background in various programming
-          languages and technologies, and have worked on a wide range of
-          projects, from web development to backend and more. I am excited to
-          share my skills and experience with you and look forward to the
-          opportunity to work on exciting new projects. Thank you for visiting
-          my portfolio!
+          I'm <span>David Portillo</span>, a software developer. Welcome to my
+          software development portfolio! I'm a highly motivated and experienced
+          developer with a passion for creating innovative and efficient
+          solutions.
+        </p>
+        <p className='faded-paragraph'>
+          With a strong background in various programming languages and
+          technologies, I've worked on a wide range of projects, from web
+          development to backend and more. I'm excited to share my skills and
+          experience with you and look forward to the opportunity to work on
+          exciting new projects together. Thank you for visiting my portfolio!
         </p>
       </div>
     </React.Fragment>
